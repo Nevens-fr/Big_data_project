f87d0da37f7389597f729007be2676ae775f4018
Nevens-fr <aurelientudo@hotmail.fr>
2022-10-12 10:54:00 +0200

    récupération des derniers tweets d'un user

---
+++ test.py
@@ -0,0 +1,30 @@
+import tweepy
+
+auth = tweepy.OAuth1UserHandler(
+   "i4ELbXGMEBNE97HB6mrmNuI91", "tWjVFA9yzSzXroZwMSsnrQBE3FQqWV1lr4RmMjSS6vUNSVwBKv", "1572835833345986563-zBLKPbqewLfVIBvTIXem6RI7cOcmQr", "VDZkOJf3e0yUC9SJvI71MS190L9GOSNFuUu5LnbMqPVgs"
+)
+#consumer_key, consumer_secret, access_token, access_token_secret
+
+auth = tweepy.OAuth2BearerHandler("AAAAAAAAAAAAAAAAAAAAAOxrhQEAAAAAJfwkToj3MYyq40OZz2KiXLDbmzo%3Dobj9h2bGR39ff7gzOQhfN9p7vOPrmzkM1iwMAThzhwQkBcH2v0")
+
+api = tweepy.API(auth)
+
+userID= "Nivvvvvvees"
+#public_tweets = api.home_timeline()
+#for tweet in public_tweets:
+#    print(tweet.text)
+
+tweets = api.user_timeline(screen_name=userID, 
+                           # 200 is the maximum allowed count
+                           count=200,
+                           include_rts = False,
+                           # Necessary to keep full_text 
+                           # otherwise only the first 140 words are extracted
+                           tweet_mode = 'extended'
+                           )
+
+for info in tweets[:3]:
+     print("ID: {}".format(info.id))
+     print(info.created_at)
+     print(info.full_text)
+     print("\n")
