c368af3ec67427f500a6326cd3e2c65e6c9ce769
Nevens <aurelientudo@hotmail.fr>
2022-10-13 14:59:07 +0100

    ajouts de premiers résultats modules à installer todo recherche par mots clefs calcul de subjectivité/polarité

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 import tweepy
 import re
+import textblob
+import codecs
+from textblob_fr import PatternTagger, PatternAnalyzer
 
 auth = tweepy.OAuth1UserHandler(
    "i4ELbXGMEBNE97HB6mrmNuI91", "tWjVFA9yzSzXroZwMSsnrQBE3FQqWV1lr4RmMjSS6vUNSVwBKv", "1572835833345986563-zBLKPbqewLfVIBvTIXem6RI7cOcmQr", "VDZkOJf3e0yUC9SJvI71MS190L9GOSNFuUu5LnbMqPVgs"
@@ -35,14 +38,56 @@ for info in tweets[:len(tweets)]:
      print("\n")
 """
 #récupération de tous les tweets contenant quoi
-mot = "quoi"
+#mot = "quoi"
+mot="#guncontrol"
+#mot ="#GPExplorer"
 q = (mot)
+q +=  " -filter:retweets"
 count = 100
-fetched_tweets = api.search_tweets(q, count = count,tweet_mode = 'extended')
+fetched_tweets = api.search_tweets(q, count = count,lang="en",tweet_mode = 'extended')
 
+
+"""regarde si un mot est a la fin du tweet
 if(None == re.search(mot+'$', fetched_tweets[0].full_text)):
    print(fetched_tweets[0].full_text)
    #affichage de l'url pour consulter le résultat dans le navigateur
    print("https://twitter.com/"+str(fetched_tweets[0].user.id) +"/status/"+str(fetched_tweets[0].id))
 else:
    print("Ne fini pas par quoi")
+"""
+
+f = codecs.open("resultats.txt", "w",  "utf-8")
+
+i = 0
+
+while i < len(fetched_tweets):
+
+   text = fetched_tweets[i].full_text
+
+   blob = textblob.TextBlob(text)
+   #fr version
+   #blob = textblob.TextBlob(text,pos_tagger=PatternTagger(), analyzer=PatternAnalyzer())
+
+
+   f.write("global polarity" + str(blob.polarity) + " subjectivity " + str(blob.subjectivity) + " tweet id : " + str(fetched_tweets[i].id) +"\n")
+   f.write("https://twitter.com/"+str(fetched_tweets[i].user.id) +"/status/"+str(fetched_tweets[i].id) + "\n")
+   f.write("Created at : " + str(fetched_tweets[i].created_at) + "\n")
+   f.write("\n")
+   f.write(fetched_tweets[i].full_text)
+   f.write("\n")
+   f.write("##################################################################################################\n")
+
+   i+=1
+
+f.close()
+
+""""
+-1 a -0.6 neg
+-0.59 a -0.2 myen neg
+-0.19 a 0.19 mitigé
+0.2 a 0.59 myen positif
+0.6 a 1 positiif
+
+
+-0.1 a 0.1 informatif
+""" 
