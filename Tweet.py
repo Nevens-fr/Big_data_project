a31e51e643505f3446f569729834711d9f3b08ad
Nevens-fr <aurelientudo@hotmail.fr>
2022-10-14 15:04:14 +0200

    Ajout du jsonifie pour la classe tweet

--- Tweet.py
+++ Tweet.py
@@ -1,10 +1,27 @@
 class Tweet:
 
     def __init__(self):
-        self.id = ""
-        self.text = ""
-        self.full_text = ""
-        self.user = ""
-        self.created_at = ""
-        self.polarity = ""
-        self.subjectivity = ""
+        self.id = "a"
+        self.text = "a"
+        self.full_text = "a"
+        self.user = "a"
+        self.created_at = "a"
+        self.polarity = "a"
+        self.subjectivity = "a"
+
+    ##
+    #Creation d'un objet json avec toutes les données de la classe
+    #TODO change le user en objet
+    def jsonified(self):
+        return {
+            "id" : self.id,
+            "text" : self.text,
+            "full_text" : self.full_text ,
+            "created_at" : self.created_at ,
+            "polarity" : self.polarity ,
+            "subjectivity" : self.subjectivity ,
+            "user" : self.user ,
+        }
+
+t = Tweet()
+print(t.jsonified())
